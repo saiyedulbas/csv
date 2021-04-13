60af27c51cb538bc1c32230e59b138fae41ddee4
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2021-04-13 19:36:26 +0600

    ijhgfds

--- index.php
+++ index.php
@@ -152,7 +152,7 @@ $(document).ready(function() {
 </head>
 
 <body>
-    <h2>Import CSV file into My fgdsrtearfgaertywert</h2>
+    <h2>I</h2>
 
     <div id="response"
         class="<?php if(!empty($type)) { echo $type . " display-block"; } ?>">
