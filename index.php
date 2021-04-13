da02d6fad29bc4f9a5cbd4385ad820b0ffee588e
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2021-04-13 20:08:17 +0600

    finally

--- index.php
+++ index.php
@@ -152,7 +152,7 @@ $(document).ready(function() {
 </head>
 
 <body>
-    <h2>juta ebong jharu</h2>
+    <h2>juta ebong jharu keno aro kisu niye asho</h2>
 
     <div id="response"
         class="<?php if(!empty($type)) { echo $type . " display-block"; } ?>">
